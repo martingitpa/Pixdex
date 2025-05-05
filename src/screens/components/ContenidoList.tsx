dd1a3e9b38940c273e2034eb7bd80f1af4ffee16
Martín Gutierrez <martinmgutierrez2003@gmail.com>
2025-05-05 18:54:49 -0300

    Arreglo en lista de contenido

--- src/screens/components/ContenidoList.tsx
+++ src/screens/components/ContenidoList.tsx
@@ -4,26 +4,31 @@ import { Link } from "expo-router";
 import { ROUTES } from "@/src/navigation/routes";
 
 export function ContenidoList() {
-  const coffeeItems = [
+  const contenidoItems = [
     {
-      name: "Flat White",
-      category: "Espresso",
-      prettyPrice: "$ 3,53",
-      rate: 4.8,
+      name: "Contenido 1",
+      image: "https://place-hold.it/170x150",
+    },
+    {
+      name: "Contenido 2",
+      image: "https://place-hold.it/170x150",
+    },
+    {
+      name: "Contenido 3",
       image: "https://place-hold.it/170x150",
     },
   ];
 
   return (
     <FlatList
-      data={coffeeItems}
+      data={contenidoItems}
       renderItem={({ item }) => (
         <Link
           href={ROUTES.CONTENIDO}
           style={{
-            backgroundColor: "white",
+            backgroundColor: "#403E43",
             borderRadius: 20,
-            width: "45%",
+            width: "30%",
             padding: 10,
             gap: 10,
           }}
@@ -32,7 +37,7 @@ export function ContenidoList() {
         </Link>
       )}
       keyExtractor={(item) => item.name}
-      numColumns={2}
+      numColumns={3}
       contentContainerStyle={styles.contentSeparator}
       columnWrapperStyle={styles.contentSeparator}
     />
